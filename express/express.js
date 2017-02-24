70a1aca238f0ab744b62f35c21e47d3e5e7f7539
Ripzery <ripzery@gmail.com>
2017-02-24 11:20:22 +0700

    ignore nodemodule

--- express/express.js
+++ express/express.js
@@ -5,12 +5,18 @@
 const connect = require('connect');
 const app = connect();
 
-const helloWorld = app.use(function(req, res, next){
-   res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+const logger = function (req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
 
-app.listen(3000);
+const helloWorld = function (req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
+
+app.use(logger);
 app.use(helloWorld);
+app.listen(3000);
 
 console.log("Server is running at http://localhost:3000");
