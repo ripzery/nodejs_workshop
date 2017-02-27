709dda120be3992c2c182021a7695b2b3e4a3faa
Ripzery <ripzery@gmail.com>
2017-02-27 10:16:55 +0700

    Add arrow function

--- day2/src/index.js
+++ day2/src/index.js
@@ -1,9 +1,22 @@
 /**
  * Created by ripzery on 2/27/17.
  */
+const colors = require('colors');
 
 // Colorful log
-let trace = require('tracer').colorConsole({level: 'info'});
-trace.info('This is info!!!');
-trace.warn('This is warning!!!');
-trace.error('This is error!!!');
+let trace = require('tracer').colorConsole({
+    level: 'info',
+});
+// trace.info('This is info!!!');
+// trace.warn('This is warning!!!');
+// trace.error('This is error!!!');
+
+/* Arrow function */
+const add = (a, b) => a + b;
+const area = (r) => {
+    const PI = 3.1416;
+    return PI * r * r
+};
+
+trace.info(add(1, 2));
+trace.info(area(3));
