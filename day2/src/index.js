a334f116970eaa0302e4fc7bd461da0edfa4a627
Ripzery <ripzery@gmail.com>
2017-02-27 10:41:47 +0700

    Add comment for module export

--- day2/src/index.js
+++ day2/src/index.js
@@ -1,22 +1,12 @@
 /**
  * Created by ripzery on 2/27/17.
  */
-const colors = require('colors');
-
+// import {add, area, PI as PIE, volume} from './math';
+import * as math from './math';
 // Colorful log
 let trace = require('tracer').colorConsole({
     level: 'info',
 });
-// trace.info('This is info!!!');
-// trace.warn('This is warning!!!');
-// trace.error('This is error!!!');
 
-/* Arrow function */
-const add = (a, b) => a + b;
-const area = (r) => {
-    const PI = 3.1416;
-    return PI * r * r
-};
+trace.info(math.add(1, 2));
 
-trace.info(add(1, 2));
-trace.info(area(3));
--- day2/src/math.js
+++ day2/src/math.js
@@ -2,6 +2,7 @@
  * Created by ripzery on 2/27/17.
  */
 
+/* Arrow function */
 /* Named export */
 const add = (a, b) => a + b;
 const PI = 3.1416;
@@ -16,3 +17,6 @@ export {
     volume,
     area
 }
+
+// Note : ใน es6 เราสามารถ export ทั้งแบบ named export และ default export ในไฟล์เดียวกันได้
+// ส่วนใน es5 เราจะเลือกได้แค่แบบใดแบบหนึ่ง ระหว่าง modules.export.ตัวแปร กับ modules.export = ฟังชั่น เป็นต้น
